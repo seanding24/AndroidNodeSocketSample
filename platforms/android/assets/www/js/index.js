d049db85f89a0ed741b7c5ef37f4db081c65eec2
xiaotian ding <xiaotian.ding.gms@gmail.com>
2014-09-22 11:44:28 -0700

    first commit

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,23 +1,57 @@
+var downloadFilePath = "cdvfile://localhost/persistent/ding/";
+var adminPageIPaddress = "http://192.168.2.8:3001";
+
+$(document).on('deviceready', function() {
+    var socket = plugin.socket.io.connect(adminPageIPaddress);
+    
+    socket.on("connect", function() {
+        alert("connected");
+        //socket.emit('message', "android");
+        socket.emit('message', {
+          'from' : "android",
+          'companyId' : "amc:torrance"
+        });
+    });
+    
+    socket.on('sendImgUrl', function(imgUrl) {
+        alert(imgUrl);
+        window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fs) {
+          downloadFile(imgUrl, "3.png");
+        }, errorHandler);
+    });
+    
+    socket.on('getImgList', function() {
+      alert(" receive event ");
+      socket.emit('androidImgList', "android");
+    });
+    
+    socket.on("error", function(err) {
+        alert(err);
+    });
+    
+});
+
+
 function downloadFile(downloadURL, savedFileName) {
     var fileTransfer = new FileTransfer();
     var uri = encodeURI(downloadURL);
-    var fileURI = 'mnt/sdcard/' + savedFileName;
+    var fileURI = downloadFilePath + savedFileName;
     
     console.log(" fileURI ==> " + fileURI);
-    
     
     fileTransfer.download(
         uri,
         fileURI,
         function(entry) {
             console.log("download complete: " + entry.toURL());
-        },
+        }, 
         function(error) {
             console.log("download error source " + error.source);
             console.log("download error target " + error.target);
             console.log("upload error code" + error.code);
         },
-        false,
+        false, 
         {
             headers: {
                 "Authorization": "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
@@ -27,25 +61,64 @@ function downloadFile(downloadURL, savedFileName) {
 };
 
 
+function fail(error) {
+    alert(error.code);
+}
 
-$(document).on('deviceready', function() {
-    alert("device ready.");
+
+function getFramesList() {
     
-    var socket = plugin.socket.io.connect("http://192.168.2.8:3001");
-    
-    socket.on("connect", function() {
-        alert("connected");
-        socket.emit('message', "android");
-    });
-    
-    socket.on('sendImgUrl', function(imgUrl) {
-        alert(imgUrl);
-        downloadFile(imgUrl, "2.png");
-    });
-    
-    socket.on("error", function(err) {
-        alert(err);
-    });
-    
-});
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
+        var sdcard = fileSystem.root;
+
+        sdcard.getDirectory('DCIM/Camera',{create:false}, function(dcim){
+            var directoryReader = dcim.createReader();
+            directoryReader.readEntries(function(entries){
+                onReadEntries (entries, fileSystem);
+            }, fail);
+
+        }, fail);
+    }, fail);
+
+    function onReadEntries (entries, fileSystem) {
+        var img_arr = [];
+        for (var i = 0; i < entries.length; i++) {
+            
+            console.log(entries[i]);
+            
+            var full_path = "/sdcard" + entries[i].fullPath;
+            var file_extension = getFileExtension(entries[i].name);
+            img_arr.push(full_path);
+        }
+        
+        console.log(" *** img_arr ***" + img_arr);
+        
+    }
+
+};
+
+function errorHandler(e) {
+    var msg = '';
+  
+    switch (e.code) {
+        case FileError.QUOTA_EXCEEDED_ERR:
+            msg = 'QUOTA_EXCEEDED_ERR';
+            break;
+        case FileError.NOT_FOUND_ERR:
+            msg = 'NOT_FOUND_ERR';
+            break;
+        case FileError.SECURITY_ERR:
+            msg = 'SECURITY_ERR';
+            break;
+        case FileError.INVALID_MODIFICATION_ERR:
+            msg = 'INVALID_MODIFICATION_ERR';
+            break;
+        case FileError.INVALID_STATE_ERR:
+            msg = 'INVALID_STATE_ERR';
+            break;
+        default:
+            msg = 'Unknown Error';
+            break;
+    };
+}
 
